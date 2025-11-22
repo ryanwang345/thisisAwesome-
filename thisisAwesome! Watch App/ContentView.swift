0f88bde46c190d68abd8582baaeb5b423d9e3488
rwang <rwang@vailsys.com>
2025-11-22 00:16:30 -0800

    Initial Commit

---
+++ thisisAwesome! Watch App/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  thisisAwesome! Watch App
+//
+//  Created by Ryan Wang on 11/22/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ thisisAwesome! Watch App/thisisAwesome_App.swift
@@ -0,0 +1,17 @@
+//
+//  thisisAwesome_App.swift
+//  thisisAwesome! Watch App
+//
+//  Created by Ryan Wang on 11/22/25.
+//
+
+import SwiftUI
+
+@main
+struct thisisAwesome__Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
